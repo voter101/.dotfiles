277a7a8a08af435f64cdf67382b84c41ad2dedd1
Wiktor Mociun <voter101@gmail.com>
2014-09-08 12:53:38 +0200

    Add global gitignore to dotfiles

--- install_osx.sh
+++ install_osx.sh
@@ -7,3 +7,8 @@ sudo brew install ctags
 sudo brew install zsh
 sudo brew install tmux
 sudo brew install reattach-to-user-namespace --wrap-pbcopy-and-pbpaste
+
+git submodule init
+git submodule update
+
+git config --global core.excludesfile '~/.gitignore-global'
