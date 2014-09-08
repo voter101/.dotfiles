6b72e6ffbda845fc09e376428ec77df408120a84
Wiktor Mociun <voter101@gmail.com>
2014-09-08 11:17:00 +0200

    Modify installer)

--- install_osx.sh
+++ install_osx.sh
@@ -3,6 +3,7 @@ ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 
 sudo brew update
 sudo brew install vim
-sudo brew install reattach-to-user-namespace --wrap-pbcopy-and-pbpaste
 sudo brew install ctags
 sudo brew install zsh
+sudo brew install tmux
+sudo brew install reattach-to-user-namespace --wrap-pbcopy-and-pbpaste
