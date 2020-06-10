a8fe709700951874a1ef737b271a49e689ca0694
Wiktor Mociun <voter101@gmail.com>
2020-06-10 17:48:36 +0200

    Update

--- install_osx.sh
+++ install_osx.sh
@@ -2,14 +2,22 @@
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 
 brew update
-brew doctor
-brew install vim
+brew install neovim/neovim/neovim
 brew install ctags
-brew install zsh
+brew install curl
+brew install wget
 brew install tmux
-brew install reattach-to-user-namespace --wrap-pbcopy-and-pbpaste
+brew install yarn
+brew install rbenv
+brew install nodenv
 
-git submodule init
-git submodule update
+# Oh My ZSH
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+cp ./.zshrc ~/.zshrc
+cp ./.gitignore-global ~/
 
 git config --global core.excludesfile '~/.gitignore-global'
+git config --global user.email "voter101@gmail.com"
+git config --global user.name "Wiktor Mociun"
+git config --global core.editor "vim"
