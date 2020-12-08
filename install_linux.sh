9de10a554e58dbbfcf001d8dec6110d16be3eaec
Wiktor Mociun <voter101@gmail.com>
2020-12-08 01:45:49 +0100

    Updates

---
+++ install_linux.sh
@@ -0,0 +1,6 @@
+cp ./.gitignore-global ~/
+
+git config --global core.excludesfile '~/.gitignore-global'
+git config --global user.email "voter101@gmail.com"
+git config --global user.name "Wiktor Mociun"
+git config --global core.editor "vim"
