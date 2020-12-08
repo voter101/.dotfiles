316bf3e26039b9705a907ee58adb187c10034645
Wiktor Mociun <voter101@gmail.com>
2020-12-08 09:39:33 +0100

    Update install script

--- install_osx.sh
+++ install_osx.sh
@@ -1,20 +1,10 @@
-# Install Brew
-ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
-
 brew update
 brew install neovim/neovim/neovim
 brew install ctags
 brew install curl
 brew install wget
 brew install tmux
-brew install yarn
-brew install rbenv
-brew install nodenv
 
-# Oh My ZSH
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-cp ./.zshrc ~/.zshrc
 cp ./.gitignore-global ~/
 
 git config --global core.excludesfile '~/.gitignore-global'
