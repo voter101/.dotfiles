8520cb3c3d51684e3a314c4f14dcf56c96a3840b
Wiktor Mociun <voter101@gmail.com>
2023-04-04 23:52:40 +0200

    Cleanup

--- install_linux.sh
+++
@@ -1,6 +0,0 @@
-cp ./.gitignore-global ~/
-
-git config --global core.excludesfile '~/.gitignore-global'
-git config --global user.email "voter101@gmail.com"
-git config --global user.name "Wiktor Mociun"
-git config --global core.editor "vim"
