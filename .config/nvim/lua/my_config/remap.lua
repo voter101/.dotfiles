7d94f6e80e48628d229196a0b9a91732f141b4ac
Wiktor Mociun <voter101@gmail.com>
2023-04-08 11:43:19 +0200

    Remaps

--- .config/nvim/lua/my_config/remap.lua
+++ .config/nvim/lua/my_config/remap.lua
@@ -25,3 +25,5 @@ vim.keymap.set("n", "Q", "<nop>")
 -- Relace selected word
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
+-- Close all other splits
+vim.keymap.set("n", "<leader>o", ":only")
