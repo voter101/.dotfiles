297c8f81bd753d841733c6fa0a88a3cb40ae0164
Wiktor Mociun <voter101@gmail.com>
2024-03-07 01:04:01 +0100

    Mac installation script

---
+++ install_script.sh
@@ -0,0 +1,10 @@
+brew install git
+brew install tmux
+brew tap homebrew/cask-fonts
+brew install --cask font-fira-mono-nerd-font
+brew install jesseduffield/lazygit/lazygit
+brew install ripgrep
+brew install fd
+brew install --cask iterm2
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
